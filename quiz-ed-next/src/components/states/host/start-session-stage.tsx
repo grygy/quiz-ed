e6d9385b03b0c732abef00376975cc9f76fbc131
Adam Grygar <adam.grygar@kiwi.com>
2024-09-01 13:28:50 +0200

    refactor: move player list to start session (#12)

--- quiz-ed-next/src/components/states/host/start-session-stage.tsx
+++ quiz-ed-next/src/components/states/host/start-session-stage.tsx
@@ -1,15 +1,21 @@
 "use client";
 
 import { HostStage } from "@/app/[gameId]/host/page";
+import { GameState } from "@/models/game-state";
 
 type Props = {
   setStage: (stage: HostStage) => void;
+  gameState: GameState;
 };
 
-const StartSessionStage = ({ setStage }: Props) => {
+const StartSessionStage = ({ setStage, gameState }: Props) => {
   return (
     <div>
       <h1>StartSessionStage</h1>
+      <div>players:</div>
+      {gameState && (
+        <div>{gameState?.players.map((player) => player.name).join(", ")}</div>
+      )}
       <button className="btn" onClick={() => setStage("hostGame")}>
         Start
       </button>
