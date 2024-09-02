2bf1e27f8d68843fc696bcfc1c5ac40fedc3b9b3
Adam Grygar <adam.grygar@kiwi.com>
2024-09-02 22:13:26 +0200

    refactor: extract countdown functionality to a new hook (#13)

--- quiz-ed-next/src/components/states/host/host-game-stage.tsx
+++ quiz-ed-next/src/components/states/host/host-game-stage.tsx
@@ -2,20 +2,14 @@
 
 import { HostStage } from "@/app/[gameId]/host/page";
 import Options from "@/components/molecules/options";
+import { goToNextQuestion, isLastQuestion } from "@/game/game-manager";
 import {
-  changeQuestionState,
-  goToNextQuestion,
-  isLastQuestion,
-} from "@/game/game-manager";
-import {
-  finishQuestion,
   getCurrentQuestion,
   getNumberOfAnswersForCurrentQuestion,
   getTotalNumberOfPlayers,
 } from "@/game/question-manager";
+import useQuestionCountdown from "@/hooks/use-question-countdown";
 import { GameState } from "@/models/game-state";
-import { useEffect } from "react";
-import { useCountdown } from "usehooks-ts";
 
 type Props = {
   setStage: (stage: HostStage) => void;
@@ -32,70 +26,11 @@ const HostGameStage = ({ setStage, gameState, updateGameState }: Props) => {
   const haveAllPlayersAnswered = numberOfAnswers === totalNumberOfPlayers;
   const isLast = isLastQuestion(gameState);
 
-  const [
+  const {
     remainingReadingTimeInSeconds,
-    {
-      startCountdown: startRemainingReadingTimeInSeconds,
-      resetCountdown: resetReadingTime,
-    },
-  ] = useCountdown({
-    countStart: currentQuestion.timeToReadInSeconds,
-  });
-
-  const [
     remainingAnswerTimeInSeconds,
-    {
-      startCountdown: startRemainingAnswerTimeInSeconds,
-      resetCountdown: resetAnswerTime,
-    },
-  ] = useCountdown({
-    countStart: currentQuestion.timeLimitInSeconds,
-  });
-
-  useEffect(() => {
-    if (
-      (gameState.currentQuestionIndex === 0,
-      gameState.questionState === "reading")
-    ) {
-      startRemainingReadingTimeInSeconds();
-    }
-  }, [
-    gameState.currentQuestionIndex,
-    gameState.questionState,
     startRemainingReadingTimeInSeconds,
-  ]);
-
-  useEffect(() => {
-    if (remainingReadingTimeInSeconds <= 0.0001) {
-      startRemainingAnswerTimeInSeconds();
-      resetReadingTime();
-      updateGameState((gameState) => {
-        return changeQuestionState(gameState, "answering");
-      });
-    }
-  }, [
-    remainingReadingTimeInSeconds,
-    startRemainingAnswerTimeInSeconds,
-    updateGameState,
-    resetReadingTime,
-  ]);
-
-  useEffect(() => {
-    if (remainingAnswerTimeInSeconds <= 0.0001 || haveAllPlayersAnswered) {
-      resetAnswerTime();
-      updateGameState((gameState) => {
-        return finishQuestion(gameState);
-      });
-    }
-  }, [
-    remainingReadingTimeInSeconds,
-    startRemainingAnswerTimeInSeconds,
-    updateGameState,
-    resetReadingTime,
-    remainingAnswerTimeInSeconds,
-    resetAnswerTime,
-    haveAllPlayersAnswered,
-  ]);
+  } = useQuestionCountdown(gameState, updateGameState, haveAllPlayersAnswered);
 
   if (gameState.state !== "playing") {
     return <h1>Invalid state {gameState.state}</h1>;
